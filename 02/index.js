38d879b3601e001b190e15749cb21352f2ac5f35
Meerim Maratova <maratovameerim@gmail.com>
2024-11-27 23:53:12 +0600

    made an API for decoding text/ DONE

--- 02/index.js
+++ 02/index.js
@@ -5,10 +5,18 @@ const port = 8000;
 
 const password = "password";
 
-
-
 app.get("/encode/:text", (req, res) => {
     const text = req.params.text;
     const encodedtext = Vigenere.Cipher(password).crypt(text);
     res.send(encodedtext);
+})
+
+app.get("/decode/:text", (req, res) => {
+    const text = req.params.text;
+    const decodedtext = Vigenere.Decipher(password).crypt(text);
+    res.send(decodedtext);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 })
